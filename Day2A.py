d84ee6d7b5feee05327b35f693ff4c698376b3c4
Olaniyan Daniel Olusegun <danielolaniyan1@gmail.com>
2022-07-08 12:05:23 +0100

    Add files via upload

---
+++ Day2A.py
@@ -0,0 +1,23 @@
+
+def main():
+    try:
+        customer_purchase = int(input('Please enter the number of books you purchased this month '))
+
+    except ValueError as error:
+        print('Invalid Number of purchase')
+
+    else:
+        if customer_purchase == 0:
+            pts = 0 
+        elif customer_purchase == 1:
+            pts = 6
+        elif customer_purchase == 2:
+            pts = 16
+        elif customer_purchase == 3:
+            pts = 32
+        elif customer_purchase >= 4:
+            pts = 60
+    print(f"You have been awarded {pts} points")
+
+while True:
+    main()
